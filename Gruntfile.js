4f00a6f06c9f0f8f71a96849e051e33b1e91d3a1
tydaeus <tydaeus@gmail.com>
2014-12-13 14:59:41 -0600

    add bower.json, exorcise maps

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,16 @@ module.exports = function(grunt)     {
             },
             options: {
                 watch: true,
-                keepAlive: true,
+                keepAlive: false,
                 browserifyOptions: {
                     debug: true
+                }
+            }
+        },
+        exorcise: {
+            build: {
+                files : {
+                    "build/main.js.map" : ["build/main.js"]
                 }
             }
         }
@@ -24,7 +31,9 @@ module.exports = function(grunt)     {
 
     grunt.loadNpmTasks("grunt-browserify");
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-exorcise");
 
     //grunt.registerTask("foo", ["clean"]);
-    grunt.registerTask("default", ["clean", "browserify"]);
+    grunt.registerTask("default", ["clean", "browserify", "exorcise"]);
+    grunt.registerTask("build", ["clean:build", "browserify:build", "exorcise:build"]);
 };
